ed8330aca4a40a0366dfa94296f96df3c218c0dc
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-10 12:55:48 -0700

    演示测试

--- test/测试.js
+++ test/测试.js
@@ -11,6 +11,7 @@ describe('魏书内容', function() {
     });
     it('不空', function() {
       for (卷名 of 演示.魏书目录()) {
+        console.log(卷名 + ": " + 演示.魏书按卷内容()[卷名].length)
         assert.ok(演示.魏书按卷内容()[卷名].length > 0);
       }
     });
@@ -27,6 +28,7 @@ describe('蜀书内容', function() {
     });
     it('不空', function() {
       for (卷名 of 演示.蜀书目录()) {
+        console.log(卷名 + ": " + 演示.蜀书按卷内容()[卷名].length)
         assert.ok(演示.蜀书按卷内容()[卷名].length > 0);
       }
     });
@@ -47,12 +49,24 @@ describe('吴书内容', function() {
     });
     it('不空', function() {
       for (卷名 of 演示.吴书目录()) {
+        console.log(卷名 + ": " + 演示.吴书按卷内容()[卷名].length)
         assert.ok(演示.吴书按卷内容()[卷名].length > 0);
       }
     });
     it('段落内容', function() {
       assert.strictEqual(演示.吴书按卷内容()[演示.吴书目录()[0]][0],
       '孙坚字文台，吴郡富春人，盖孙武之后也。');
+    });
+  });
+});
+
+describe('演示', function() {
+  describe('', function() {
+    it('目录', function() {
+      console.log(演示.魏书目录());
+    });
+    it('段落内容', function() {
+      console.log(演示.蜀书按卷内容()['刘二牧传']);
     });
   });
 });
