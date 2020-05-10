3922fc84b104427655f8dea356d723de01557a6b
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-10 01:29:10 -0700

    吴书

--- test/测试.js
+++ test/测试.js
@@ -9,6 +9,11 @@ describe('魏书内容', function() {
     it('段落数', function() {
       assert.strictEqual(演示.魏书按卷内容()[演示.魏书目录()[0]].length, 153);
     });
+    it('不空', function() {
+      for (卷名 of 演示.魏书目录()) {
+        assert.ok(演示.魏书按卷内容()[卷名].length > 0);
+      }
+    });
   });
 });
 
@@ -20,9 +25,34 @@ describe('蜀书内容', function() {
     it('段落数', function() {
       assert.strictEqual(演示.蜀书按卷内容()[演示.蜀书目录()[0]].length, 9);
     });
+    it('不空', function() {
+      for (卷名 of 演示.蜀书目录()) {
+        assert.ok(演示.蜀书按卷内容()[卷名].length > 0);
+      }
+    });
     it('段落内容', function() {
       assert.strictEqual(演示.蜀书按卷内容()[演示.蜀书目录()[0]][4],
       '焉徙治成都，既痛其子，又感祅灾，兴平元年，痈疽发背而卒。州大吏赵韪等贪璋温仁，共上璋为益州刺史，诏书因以为监军使者，领益州牧，以韪为征东中郎将，率众击刘表。');
+    });
+  });
+});
+
+describe('吴书内容', function() {
+  describe('', function() {
+    it('目录', function() {
+      assert.strictEqual(演示.吴书目录().length, 20);
+    });
+    it('段落数', function() {
+      assert.strictEqual(演示.吴书按卷内容()[演示.吴书目录()[0]].length, 21);
+    });
+    it('不空', function() {
+      for (卷名 of 演示.吴书目录()) {
+        assert.ok(演示.吴书按卷内容()[卷名].length > 0);
+      }
+    });
+    it('段落内容', function() {
+      assert.strictEqual(演示.吴书按卷内容()[演示.吴书目录()[0]][0],
+      '孙坚字文台，吴郡富春人，盖孙武之后也。');
     });
   });
 });
--- 入口.js
+++ 入口.js
@@ -4,11 +4,19 @@ for (卷名 of 魏书目录.三十卷) {
   const 卷 = require('./魏书/' + 卷名)
   魏书[卷名] = 卷.内容
 }
+
 const 蜀书目录 = require('./蜀书/目录')
 const 蜀书 = {}
 for (卷名 of 蜀书目录.十五卷) {
   const 卷 = require('./蜀书/' + 卷名)
   蜀书[卷名] = 卷.内容
+}
+
+const 吴书目录 = require('./吴书/目录')
+const 吴书 = {}
+for (卷名 of 吴书目录.二十卷) {
+  const 卷 = require('./吴书/' + 卷名)
+  吴书[卷名] = 卷.内容
 }
 
 exports.魏书目录 = function() {
@@ -25,6 +33,14 @@ exports.蜀书目录 = function() {
 
 exports.蜀书按卷内容 = function() {
   return 蜀书;
+}
+
+exports.吴书目录 = function() {
+  return 吴书目录.二十卷;
+}
+
+exports.吴书按卷内容 = function() {
+  return 吴书;
 }
 
 //console.log(魏书.内容[魏书.目录[0]].join("\n\n"))
